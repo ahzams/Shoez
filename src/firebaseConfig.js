62d8d2c2ab1a756bc96ff7b6282624c9d34b8d7e
Ahzam Syed <ahzam183@gmail.com>
2022-09-14 19:48:51 +0530

    updated

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,7 @@
 
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAOBrV6Xn3ggo8WUITKqJL11hEldzOAiH4",
